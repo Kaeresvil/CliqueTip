aeb75073f9d4eb89973630a1060b90eca7bf36ee
Queenee Vidad <qvidad@gmail.com>
2022-04-07 23:00:26 +0800

    Delete previous chatroom

--- resources/views/chatroom.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <!-- Main Content -->
-    <main class="main-content">
-        <div class="container">
-            <h1>HELLO CHATROOM</h1>
-        </div>
-    </main>
-@endsection
