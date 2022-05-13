6427105a68fdae1cd9e87a69ea8debaf984c1145
pr_borja <pborja396@gmail.com>
2022-05-13 23:15:43 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,47 @@
 @extends('layouts.app')
 
+@section('css')
+<style>
+    .myimg{
+        width:150px;
+        height:150px;
+        object-fit:cover;
+    }
+</style>
+@endsection
+
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <div class="d-flex justify-content-start p-1">
+                        <div class="border" style="margin-left: 10%;">
+                            <img src="{{ asset('uploads/profile/Patrick.jpg')}}" class="img-circle elevation-2 myimg" alt="User Image" style="border-radius: 50%;">
+                        </div>
+                        <div class="border" style="margin-left: 10%;"> 
+                            <li>
+                                Full Name
+                            </li>
+                            <li>
+                                Edit Profile
+                            </li>
+                            <li>
+                                Account Setting
+                            </li>
+                        </div>  
+                        <div class="border" style="margin-left: 10%; width: 30%;"> 
+                            <li>
+                                Interests
+                            </li>
+                            <li>
+                                Description
+                            </li>
+                        </div>  
+                    </div>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +50,9 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Post!') }}
+                    {{ __('Feeds!') }}
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    @yield('css')
+
 </head>
 <header>
     @include('layouts.header')
