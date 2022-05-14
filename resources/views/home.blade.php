0df34fcd2de5beddaae04c87254206eda6f49fe5
pr_borja <pborja396@gmail.com>
2022-05-14 15:34:05 +0800

    Update-Patrick

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('css')
 <style>
     .myimg{
-        width:150px;
-        height:150px;
+        width:125px;
+        height:125px;
         object-fit:cover;
     }
 </style>
@@ -17,28 +17,33 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <div class="d-flex justify-content-start p-1">
+                    <div class="d-flex justify-content-start p-1 ">
                         <div class="border" style="margin-left: 10%;">
                             <img src="{{ asset('uploads/profile/Patrick.jpg')}}" class="img-circle elevation-2 myimg" alt="User Image" style="border-radius: 50%;">
+                            <h4 class="text-center mt-2 ">{{Auth::user()->name}}</h4>
                         </div>
-                        <div class="border" style="margin-left: 10%;"> 
-                            <li>
-                                Full Name
-                            </li>
-                            <li>
-                                Edit Profile
-                            </li>
-                            <li>
-                                Account Setting
-                            </li>
-                        </div>  
-                        <div class="border" style="margin-left: 10%; width: 30%;"> 
-                            <li>
-                                Interests
-                            </li>
-                            <li>
-                                Description
-                            </li>
+                        <div class="border d-flex align-items-center" style="margin-left: 10%;">
+                            <div class=" d-flex align-items-start flex-column" > 
+                                <div class="">
+                                    <a href="">Edit Profile</a>
+                                </div>
+                                <div class="mt-3">
+                                    <a href="">Account Setting</a>
+                                </div>
+                            </div>  
+                        </div>
+
+                        <div class="border d-flex flex-column" style="margin-left: 10%; width: 40%;"> 
+                            <div class="border p-2">
+                                <h6 class="text-center">Interests</h6>
+                                <h6 class="text-center">#Interests</h6>
+                            </div>
+                            <div class="border p-2">
+                                <h6 class="text-center">Description</h6>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                </p>
+                            </div>
                         </div>  
                     </div>
                 </div>
