aec2179f351b2716dcc7991770896a9bebbc864b
pr_borja <pborja396@gmail.com>
2022-05-15 01:24:12 +0800

    Update-Patrick

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('description');
             $table->rememberToken();
             $table->timestamps();
         });
